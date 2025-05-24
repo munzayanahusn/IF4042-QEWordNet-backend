b041fe6c1d8b0101a0ca2b57e305eab057f3b302
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2025-05-24 12:48:40 +0700

    feat: cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import nltk
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from nltk.data import find
-from nltk import download
 
 try:
     find("tokenizers/punkt")
@@ -23,6 +23,15 @@ except LookupError:
 from app.api.endpoints import user, upload, document_collection, search
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(upload.router)
 app.include_router(document_collection.router)
